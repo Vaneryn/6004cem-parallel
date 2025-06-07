923b45c06e25ca69f8d7e530a853fcccf54176a6
Vaneryn <wzthor2002@gmail.com>
2025-06-07 16:56:51 +0800

    update(OpenMP): add performance test for imbalanced workload per iteration

--- openmp/openmp_partb_schedule.cpp
+++ openmp/openmp_partb_schedule.cpp
@@ -1,13 +1,15 @@
+#include <chrono>
 #include <iomanip>
 #include <iostream>
 #include <omp.h>
 #include <string>
+#include <thread>
 #include <vector>
 
 /**
  * @brief Initializes a vector with a fixed sized and value.
  * 
- * Fills the given vector with `size number of elements, each initialized to `value`.
+ * Fills the given vector with `size` number of elements, each initialized to `value`.
  * 
  * @param vect The vector to initialize.
  * @param size The number of elements to assign.
@@ -126,12 +128,13 @@ void runSchedule(const std::vector<int>& vect1, const std::vector<int>& vect2, s
  * @param vect2 Second input vector.
  * @param vect3 Output vector to store results.
  * @param scheduleType The scheduling method to use ("static" or "dynamic").
+ * @param isBalanced Determines whether to run the test with balanced or imbalanced workload per iteration.
  * @param numThreads Optional number of threads (0 -> use all available processors).
  * @param chunkSize Optional chunk size for scheduling (currently not used).
  * @return Elapsed time in seconds.
  */
 double measureSchedule(const std::vector<int>& vect1, const std::vector<int>& vect2, std::vector<int>& vect3,
-    const std::string& scheduleType, int numThreads = 0, int chunkSize = 0) {
+    const std::string& scheduleType, bool isBalanced, int numThreads = 0, int chunkSize = 0) {
     // Set default number of threads
     if (numThreads == 0)
         numThreads = omp_get_num_procs();
@@ -142,21 +145,51 @@ double measureSchedule(const std::vector<int>& vect1, const std::vector<int>& ve
     double endTime;
 
     if (scheduleType == "static") {
-        startTime = omp_get_wtime();
-        #pragma omp parallel for schedule(static) num_threads(numThreads)
-        for (int i = 0; i < kSize; ++i) {
-            int tid = omp_get_thread_num();
-            vect3[i] = vect1[i] + vect2[i];
+        // Static scheduling
+        if (isBalanced) {
+            // Balanced workload per iteration
+            startTime = omp_get_wtime();
+            #pragma omp parallel for schedule(static) num_threads(numThreads)
+            for (int i = 0; i < kSize; ++i) {
+                vect3[i] = vect1[i] + vect2[i];
+            }
+            endTime = omp_get_wtime();
+        } else {
+            // Imbalanced workload per iteration
+            startTime = omp_get_wtime();
+            #pragma omp parallel for schedule(static) num_threads(numThreads)
+            for (int i = 0; i < kSize; ++i) {
+                vect3[i] = vect1[i] + vect2[i];
+
+                // Simulate imbalanced workload
+                if (i % 100 == 0)
+                    std::this_thread::sleep_for(std::chrono::microseconds(1));
+            }
+            endTime = omp_get_wtime();
         }
-        endTime = omp_get_wtime();
     } else if (scheduleType == "dynamic") {
-        startTime = omp_get_wtime();
-        #pragma omp parallel for schedule(dynamic) num_threads(numThreads)
-        for (int i = 0; i < kSize; ++i) {
-            int tid = omp_get_thread_num();
-            vect3[i] = vect1[i] + vect2[i];
+        // Dynamic scheduling
+        if (isBalanced) {
+            // Balanced workload per iteration
+            startTime = omp_get_wtime();
+            #pragma omp parallel for schedule(dynamic) num_threads(numThreads)
+            for (int i = 0; i < kSize; ++i) {
+                vect3[i] = vect1[i] + vect2[i];
+            }
+            endTime = omp_get_wtime();
+        } else {
+            // Imbalanced workload per iteration
+            startTime = omp_get_wtime();
+            #pragma omp parallel for schedule(dynamic) num_threads(numThreads)
+            for (int i = 0; i < kSize; ++i) {
+                vect3[i] = vect1[i] + vect2[i];
+
+                // Simulate imbalanced workload
+                if (i % 100 == 0)
+                    std::this_thread::sleep_for(std::chrono::microseconds(1));
+            }
+            endTime = omp_get_wtime();
         }
-        endTime = omp_get_wtime();
     }
 
     return (endTime - startTime);
@@ -165,10 +198,18 @@ double measureSchedule(const std::vector<int>& vect1, const std::vector<int>& ve
 int main() {
     /**
      * OUTLINE: Split program into 2 sections
-     * @section 1: Compare distribution of iterations amongst threads with low iteration count (e.g. 12)
-     * - run with default chunk size (it does not default to 1, it splits the iterations into as evenly sized chunks as possible)
-     * - run with specified chunk size (to seee round robin behavior)
-     * @section 2: Compare execution time with increasing iteration count
+     * @section 1: Scheduling Behaviour
+     * Compare distribution of iterations amongst threads with a very low iteration count (e.g. 12) to visually inspect how chunks are assigned.
+     * Static:
+     * - run with default chunk size (each thread gets one chunk; iterations are divided into as evenly sized chunks as possible)
+     * - run with specified chunk size (block-cyclic behavior/round roubin if size is 1)
+     * Dynamic:
+     * - run with default chunk size (1; threads request single iterations to process as they become available)
+     * - run with specified chunk size (threads request fixed-size chunks to process as they become available)
+     * @section 2: Performance Comparison
+     * Compare execution time with increasing iterations
+     * - run with balanced workload per iteration
+     * - run with imbalanced workload per iteration
      */
 
     // Console UI elements
@@ -176,7 +217,7 @@ int main() {
     const std::string kSingleLine(kLineLength, '-');
     const std::string kDoubleLine(kLineLength, '=');
     const std::vector<std::string> kScheduleHeaders = {"TID", "Iteration", "Result"};
-    const std::vector<std::string> kPerformanceHeaders = {"Size", "Static Total (s)", "Dynamic Total (s)", "Static Average (s)", "Dyanmic Average (s)"};
+    const std::vector<std::string> kPerformanceHeaders = {"Size", "Static Total (s)", "Dynamic Total (s)", "Static Average (s)", "Dynamic Average (s)"};
     const std::vector<int> kScheduleColWidths = {10, 15, 10};
     const std::vector<int> kPerformanceColWidths = {10, 20, 20, 22, 22};
 
@@ -222,21 +263,21 @@ int main() {
 
     // Static Scheduling
     // Default chunk size
-    std::cout << "\n(1.1) Static Scheduling - Default Chunk Size\n" << kSingleLine << std::endl;
+    std::cout << "\n[1.1] Static Scheduling - Default Chunk Size\n" << kSingleLine << std::endl;
     printTableHeader(kScheduleHeaders, kScheduleColWidths, 50);
     runSchedule(vect1, vect2, vect3, "static", kScheduleColWidths, kNumThreads);
     // Specified chunk size
-    std::cout << "\n(1.2) Static Scheduling - Specified Chunk Size [" << kStaticChunkSize << "]\n" << kSingleLine << std::endl;
+    std::cout << "\n[1.2] Static Scheduling - Specified Chunk Size (" << kStaticChunkSize << ")\n" << kSingleLine << std::endl;
     printTableHeader(kScheduleHeaders, kScheduleColWidths, 50);
     runSchedule(vect1, vect2, vect3, "static", kScheduleColWidths, kNumThreads, kStaticChunkSize);
 
     // Dynamic schedule
     // Default chunk size
-    std::cout << "\n(2.1) Dynamic Scheduling - Default Chunk Size\n" << kSingleLine << std::endl;
+    std::cout << "\n[2.1] Dynamic Scheduling - Default Chunk Size\n" << kSingleLine << std::endl;
     printTableHeader(kScheduleHeaders, kScheduleColWidths, 50);
     runSchedule(vect1, vect2, vect3, "dynamic", kScheduleColWidths, kNumThreads);
     // Specified chunk size
-    std::cout << "\n(2.2) Dynamic Scheduling - Specified Chunk Size [" << kDynamicChunkSize << "]\n" << kSingleLine << std::endl;
+    std::cout << "\n[2.2] Dynamic Scheduling - Specified Chunk Size (" << kDynamicChunkSize << ")\n" << kSingleLine << std::endl;
     printTableHeader(kScheduleHeaders, kScheduleColWidths, 50);
     runSchedule(vect1, vect2, vect3, "dynamic", kScheduleColWidths, kNumThreads, kDynamicChunkSize);
 
@@ -257,7 +298,8 @@ int main() {
         << "Vector2 value: " << kValue2 << std::endl
         << "Vector3 value: " << kValue3 << std::endl;
 
-    std::cout << "\nPerformance Over Increasing Workloads\n" << kSingleLine << kSingleLine << std::endl;
+    // Balanced Workload per Iteration
+    std::cout << "\n[1] Performance Over Increasing Iterations (Balanced)\n" << kSingleLine << kSingleLine << std::endl;
     printTableHeader(kPerformanceHeaders, kPerformanceColWidths, 100);
 
     // Conduct comparison over increasing vector sizes
@@ -274,8 +316,34 @@ int main() {
         // Repeat test with current size for consistent average results
         for (int j = 0; j < kTestCount; j++) {
             // Measure execution time for static and dynamic scheduling
-            staticTime += measureSchedule(vect1, vect2, vect3, "static", kNumThreads);
-            dynamicTime += measureSchedule(vect1, vect2, vect3, "dynamic", kNumThreads);
+            staticTime += measureSchedule(vect1, vect2, vect3, "static", true, kNumThreads);
+            dynamicTime += measureSchedule(vect1, vect2, vect3, "dynamic", true, kNumThreads);
+        }
+
+        printTableRow({std::to_string(i), std::to_string(staticTime), std::to_string(dynamicTime),
+            std::to_string(staticTime / kTestCount), std::to_string(dynamicTime / kTestCount)}, kPerformanceColWidths);
+    }
+
+    // Imbalanced Workload per Iteration
+    std::cout << "\n[2] Performance Over Increasing Iterations (Imbalanced)\n" << kSingleLine << kSingleLine << std::endl;
+    printTableHeader(kPerformanceHeaders, kPerformanceColWidths, 100);
+
+    // Conduct comparison over increasing vector sizes
+    for (int i = kStartSize; i <= kMaxSize; i *= kSizeMultiplication) {
+        const int kCurrentSize = i;
+        double staticTime = 0;
+        double dynamicTime = 0;
+
+        // Initialize vectors
+        initVector(vect1, kCurrentSize, kValue1);
+        initVector(vect2, kCurrentSize, kValue2);
+        initVector(vect3, kCurrentSize, kValue3);
+
+        // Repeat test with current size for consistent average results
+        for (int j = 0; j < kTestCount; j++) {
+            // Measure execution time for static and dynamic scheduling
+            staticTime += measureSchedule(vect1, vect2, vect3, "static", false, kNumThreads);
+            dynamicTime += measureSchedule(vect1, vect2, vect3, "dynamic", false, kNumThreads);
         }
 
         printTableRow({std::to_string(i), std::to_string(staticTime), std::to_string(dynamicTime),
